dd1118d31dd43cbb6a7bbfd3b99e992a540b347e
ayush <rakholiyaayush861@gmail.com>
2025-04-09 18:00:47 -0400

    i have created the models

--- dj_ajax/src/posts_proj/settings.py
+++ dj_ajax/src/posts_proj/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_bootstrap4'
     'posts',
     'profiles',
     'crispy_forms',
+    'crispy_bootstrap4',
 
 ]
 
