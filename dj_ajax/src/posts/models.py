21a4e171e898481ec6998c465fd848935b23424b
ayush <rakholiyaayush861@gmail.com>
2025-04-09 22:15:36 -0400

    here i have created and add first view and Template for the project

--- dj_ajax/src/posts/models.py
+++ dj_ajax/src/posts/models.py
@@ -6,7 +6,7 @@ from profiles.models import Profile
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TimeField()
+    body = models.TextField()
     liked = models.ManyToManyField(User, blank=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
@@ -14,3 +14,4 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title)
+    
