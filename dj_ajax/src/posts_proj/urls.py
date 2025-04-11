5f799f696acb084df0fa50029ae8f27b833345d4
ayush <rakholiyaayush861@gmail.com>
2025-04-11 15:10:13 -0400

    make myprfile view registration and also add the template of it

--- dj_ajax/src/posts_proj/urls.py
+++ dj_ajax/src/posts_proj/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+
 ]
 
 if settings.DEBUG:
